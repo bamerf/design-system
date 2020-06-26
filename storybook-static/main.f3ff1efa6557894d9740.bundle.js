4e17b2919da7d086c0f9f4d052cf821131b51a55
bamerf <erfanianbamdad@gmail.com>
2020-06-26 12:10:27 +1000

    Added first build

---
+++ storybook-static/main.f3ff1efa6557894d9740.bundle.js
@@ -0,0 +1,22 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1468:function(a,o,n){"use strict";n.r(o);var e=n(58),t=n(489);const r="#85BF31";var s=n(490),i=n(46);s.a`
+  ${i.normalize}
+  html {
+    font-size: 14px;
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    /* margin: 0; */
+    margin: 10px;
+    font-family: ${'"Lato", monospace'}
+  }
+
+  main {
+    width: 90%;
+    margin: 0 auto;
+  }
+`;var c=Object(t.create)({base:"dark",colorPrimary:r,colorSecondary:"deepskyblue",barBg:"#464649",brandUrl:"https://www.leadchat.com/",brandImage:"https://leadchat-brand-assets.s3-ap-southeast-2.amazonaws.com/leadchat-logo--300px.png"});e.addons.setConfig({theme:c})},493:function(a,o,n){n(494),n(641),n(1468),n(1295),n(1405),n(1407),n(1440),n(1443),n(1450),a.exports=n(1455)},560:function(a,o){}},[[493,1,2]]]);
